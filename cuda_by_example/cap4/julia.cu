8b0e9ee9773431b0e1f8f1852f0cf08c7cc8dd33
ecalasans <e1906d0103>
2021-01-18 05:17:08 -0300

    Escrevendo exemplo do cap4

---
+++ cuda_by_example/cap4/julia.cu
@@ -0,0 +1,61 @@
+//
+// Created by eric on 18/01/2021.
+//
+
+#include "../exemplos/common/book.h"
+#include "../exemplos/common/cpu_bitmap.h"
+
+#define DIM 1000
+
+struct cuComplex{
+    float r;
+    float i;
+
+    cuComplex(float a, float b) : r(a), i(b) {};
+
+    __device__ float magnitude2(){
+        return r*r + i*i;
+    }
+
+    __device__ cuComplex operator*(const cuComplex& a){
+        return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
+    }
+
+    __device__ cuComplex operator+(const cuComplex& a){
+        return cuComplex(r + a.r, i + a.i);
+    }
+};
+
+__device__ int julia(int x, int y, cuComplex& semente){
+    const float escala = 1.5;
+
+    float jx = escala * (float)(DIM/2 - x)/(DIM/2);
+    float jx = escala * (float)(DIM/2 - y)(DIM/2);
+
+    cuComplex a(jx, jy);
+
+    for (int i=0; i < 200; ++i){
+        a = a*a + semente;
+
+        if(a.magnitude2() > 1000)
+            return 0;
+    }
+
+    return 1;
+}
+
+__global__ void kernel(unsigned char* ptr, cuComplex& semente){
+    int x = blockIdx.x;
+    int y = blockIdx.y;
+    int offset = x + y*gridDim.x;
+
+    int julia_valor = julia(x, y, semente);
+
+    ptr[offset*4 + 0] = 255*julia_valor;
+    ptr[offset*4 + 1] = 0;
+    ptr[offset*4 + 2] = 0;
+    ptr[offset*4 + 3] = 255;
+
+}
+
+
